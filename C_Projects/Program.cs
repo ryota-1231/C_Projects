d19d97569d5731353112bb81c6785487d8a99ba4
竜太 青柳 <rrrrr.1231@gmail.com>
2019-12-08 17:17:19 +0900

    分岐学習（if）１

--- C_Projects/Program.cs
+++ C_Projects/Program.cs
@@ -10,6 +10,13 @@ namespace C_Projects
     {
         static void Main(string[] args)
         {
+            var line = Console.ReadLine();
+            var num = int.Parse(line);
+            if (num >= 10)
+            {
+                Console.WriteLine("num >= 10が成り立ちました");
+                Console.WriteLine($"numの値は{num}です");
+            }
         }
     }
 }
