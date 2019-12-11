54226d9e6405f5fd0a7c9ef565d1d0c22dc5a996
竜太 青柳 <rrrrr.1231@gmail.com>
2019-12-11 12:56:08 +0900

    Card作成

---
+++ Card_prj/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Card_prj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
