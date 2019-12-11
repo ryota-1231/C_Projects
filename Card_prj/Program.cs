48f12abff50203bead70cfe2ac9962fe1a4e379e
竜太 青柳 <rrrrr.1231@gmail.com>
2019-12-11 13:06:11 +0900

    構造型

--- Card_prj/Program.cs
+++ Card_prj/Program.cs
@@ -10,6 +10,49 @@ namespace Card_prj
     {
         static void Main(string[] args)
         {
+            var card = new Card('H', 8);
+            card.Print();
+            if (card.Suit == 'D')
+            {
+                Console.WriteLine("ダイヤです");
+            }
+            else
+            {
+                Console.WriteLine("ダイヤではありません");
+            }
+        }
+    }
+
+    struct Card
+    {
+        public char Suit { get; private set; }
+        public int Number { get; private set; }
+
+        public Card(char suit, int number)
+        {
+            Suit = suit;
+            Number = number;
+        }
+
+        public void Print()
+        {
+            var s = "";
+            switch (Suit)
+            {
+                case 'H':
+                    s = "ハート";
+                    break;
+                case 'D':
+                    s = "ダイヤ";
+                    break;
+                case 'S':
+                    s = "スペード";
+                    break;
+                case 'C':
+                    s = "クラブ";
+                    break;
+            }
+            Console.WriteLine($"{s} {Number}");
         }
     }
 }
