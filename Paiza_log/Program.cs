cc82b4bf760674ce57c2b8f0a052b61c52d422f0
ryota-1231 <rrrrr.1231@gmail.com>
2019-12-16 14:39:50 +0900

    DおよびCテスト

--- Paiza_log/Program.cs
+++ Paiza_log/Program.cs
@@ -155,6 +155,82 @@ namespace Paiza_log
             //var line = Console.ReadLine();
             //var y = line.Replace("n", "");
             //Console.WriteLine(y.Length);
+
+            //家族で分ける
+            // var line = System.Console.ReadLine();
+            //var nums = System.Console.ReadLine();
+            //var lists = nums.Split(' ');
+            //var h = int.Parse(lists[0]);
+            //var w = int.Parse(lists[1]);
+            //var result = h * w % int.Parse(line);
+            //System.Console.WriteLine(result);
+
+            //ワインのキャッチコピー
+            //var line = System.Console.ReadLine();
+            //var str = "Best in";
+            //System.Console.WriteLine($"{str} {line}");
+
+            //自動でチャージ
+            //var line = System.Console.ReadLine();
+            //var num = int.Parse(line);
+            //if (num >= 10000)
+            //{
+            //    System.Console.WriteLine(num);
+            //}
+            //else
+            //{
+            //    System.Console.WriteLine(num + 10000);
+            //}
+
+            //西暦の計算
+            //var line = System.Console.ReadLine();
+            //var nums = line.Split(' ');
+            //var num1 = int.Parse(nums[0]);
+            //var num2 = int.Parse(nums[1]);
+            //System.Console.WriteLine(num2 - num1);
+
+            //カード並べ
+            //var line = Console.ReadLine();
+            //var numbers = line.Split(' ');
+            //int[] intArray = numbers.Select(int.Parse).ToArray();
+            //Array.Sort(intArray);
+            //var num1 = intArray[3] * 10;
+            //var num2 = intArray[2] * 10;
+            //var result = num1 + num2 + intArray[0] + intArray[1];
+            //Console.WriteLine(result);
+
+
+            //テストの採点
+            //var line = Console.ReadLine();
+            //var lines = line.Split(' ');
+            //int[] lists = lines.Select(int.Parse).ToArray();
+            //var members = new List<int>();
+            //for (var i = 0; i < lists[0]; i++)
+            //{
+            //    var eles = Console.ReadLine();
+            //    var ele = eles.Split(' ');
+            //    var nums = ele.Select(int.Parse).ToArray();
+            //    if (lists[1] != 0)
+            //    {
+            //        if ((nums[0] - nums[1] * 5 - lists[1]) < 0)
+            //        {
+            //            continue;
+            //        }
+            //        else
+            //        {
+
+            //            members.Add(i + 1);
+            //        }
+            //    }
+            //    else
+            //    {
+            //        members.Add(i + 1);
+            //    }
+            //}
+            //foreach (var mem in members)
+            //{
+            //    Console.WriteLine(mem);
+            //}
         }
     }
 }
